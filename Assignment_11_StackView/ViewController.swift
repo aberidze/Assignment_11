7c29d2b7fb241b27ef878eca99568ac522cce284
Anri_Beridze <aberidze02@gmail.com>
2023-11-04 12:21:20 +0400

    Initial Commit

---
+++ Assignment_11_StackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_11_StackView
+//
+//  Created by Macbook Air 13 on 04.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
